183c2291ee296d1c906c0f3831c88623ab652202
Nazanin <earworm90@gmail.com>
2023-03-14 16:51:06 +0200

    use gpt3 api

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
-import { API_TOKEN } from "./config.js";
+import { API_TOKEN, OPENAI_TOKEN } from "./config.js";
 
-const MODEL = "facebook/bart-large-cnn";
+// const MODEL = "facebook/bart-large-cnn";
 
 chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
   const currentTab = tabs[0];
@@ -9,12 +9,18 @@ chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
   displayInformation("", "");
 
   const tabInformation = await getTabInformation(currentTab);
-  const modelResult = await runModel(MODEL, tabInformation["textToSummarise"]);
+  // const modelResult = await runModel(MODEL, tabInformation["textToSummarise"]);
+  const summerizeResult = await summarizeText(
+    tabInformation["textToSummarise"],
+    OPENAI_TOKEN
+  );
 
   toggleLoader(false);
   displayInformation(
     tabInformation["textTitle"],
-    modelResult[0]["summary_text"]
+    // modelResult[0]["summary_text"],
+
+    summerizeResult
   );
 });
 
@@ -55,23 +61,52 @@ async function getTabInformation(tab) {
 
 // // Using the API
 
-async function runModel(model, text) {
-  console.log("run model is almost working");
-  /**
-   * Sends a request to a specified summarisation model given some text to summarise.
-   **/
-  const data = JSON.stringify({ inputs: text });
-  const endpoint = `https://api-inference.huggingface.co/models/${model}`;
+// async function runModel(model, text) {
+//   console.log("run model is almost working");
+//   /**
+//    * Sends a request to a specified summarisation model given some text to summarise.
+//    **/
+//   const data = JSON.stringify({ inputs: text });
+//   const endpoint = `https://api-inference.huggingface.co/models/${model}`;
 
-  const response = await fetch(endpoint, {
-    headers: { Authorization: `Bearer ${API_TOKEN}` },
-    method: "POST",
-    body: data,
+//   const response = await fetch(endpoint, {
+//     headers: { Authorization: `Bearer ${API_TOKEN}` },
+//     method: "POST",
+//     body: data,
+//   });
+
+//   const result = await response.json();
+
+//   return result;
+// }
+
+// USING GPT3 API FROM OPENAI
+
+async function summarizeText(text, OPENAI_TOKEN) {
+  return new Promise((resolve, reject) => {
+    fetch("https://api.openai.com/v1/engines/davinci-codex/completions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${OPENAI_TOKEN}`,
+      },
+      body: JSON.stringify({
+        prompt: `summarize this text and display with the main headlines such as background, method, results: ${text}`,
+        max_tokens: 300,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const summary = data.choices[0].text.trim();
+        // Do something with the summary
+        console.log(summary);
+        resolve(summary);
+      })
+      .catch((error) => {
+        console.error(error);
+        reject(error);
+      });
   });
-
-  const result = await response.json();
-
-  return result;
 }
 
 function displayInformation(title, summary) {
