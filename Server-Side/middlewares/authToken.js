1711cd3df37b6c0a7e2e46195e00f6348c459703
Nazanin <earworm90@gmail.com>
2023-05-29 16:53:16 +0300

    add middleware authToken

---
+++ Server-Side/middlewares/authToken.js
@@ -0,0 +1,31 @@
+const jwt = require("jsonwebtoken");
+User = require("../models/user");
+
+const verifyToken = (req, res, next) => {
+  if (
+    req.headers &&
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "JWT"
+  ) {
+    jwt.verify(
+      req.headers.authorization.split(" ")[1],
+      process.env.API_SECRET,
+      function (err, decode) {
+        if (err) req.user = undefined;
+        User.findOne({ _id: decode.id })
+          .exec()
+          .then((user) => {
+            req.user = user;
+            next();
+          })
+          .catch((err) => {
+            res.status(500).send({ message: err });
+          });
+      }
+    );
+  } else {
+    req.user = undefined;
+    next();
+  }
+};
+module.exports = verifyToken;
