a2fa74df8152e7ffbb9b31b4814fcee9aa032780
nawzneen <earworm90@gmail.com>
2023-06-04 17:15:21 +0300

    Add user model

---
+++ Server-Side/models/user.js
@@ -0,0 +1,43 @@
+var mongoose = require("mongoose"),
+  Schema = mongoose.Schema;
+
+// User Schema
+var userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required!"],
+    },
+    email: {
+      type: String,
+      unique: [true, "Email already exists!"],
+      lowercase: true,
+      trim: true,
+      required: [true, "Email not provided!"],
+      validate: {
+        validator: function (v) {
+          return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+        },
+        message: "{VALUE} is not a valid email!",
+      },
+    },
+    role: {
+      type: String,
+      enum: ["tester", "admin"],
+      required: [true, "Please specify a user role"],
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    created: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    collection: "users",
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
