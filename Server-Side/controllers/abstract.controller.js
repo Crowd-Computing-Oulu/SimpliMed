166a2bbf639b00c7df3d6178d41ea572303883b1
nawzneen <earworm90@gmail.com>
2023-06-04 12:27:42 +0300

    add a test code

--- Server-Side/controllers/abstract.controller.js
+++ Server-Side/controllers/abstract.controller.js
@@ -1,15 +1,36 @@
-const User = require("../models/user");
+// const User = require("../models/user");
 const Abstract = require("../models/abstract");
 const Interaction = require("../models/interaction");
+const { sendHttpRequest, sendHttpsRequest } = require("../utils/requestUtils");
+
+// Delete Later
+exports.test = async (req, res) => {
+  sendHttpRequest({
+    hostname: "localhost",
+    port: 8080,
+    path: "/api",
+    method: "POST",
+    headers: {},
+  })
+    .then((data) => {
+      console.log(data.message);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+
+  res.status(200).send({ message: "Done" });
+};
 
 exports.requestAbstract = async (req, res) => {
   console.log("Abstract Requested.");
 
-  abstract = null;
+  let abstract = null;
   await Abstract.findOne({ url: req.body.url })
     .exec()
     .then((anAbstract) => {
       abstract = anAbstract;
+      // Checking if the abstract already exist in the databas
       if (anAbstract != null) {
         console.log("Abstract Already Exists. ", abstract);
       }
@@ -19,10 +40,10 @@ exports.requestAbstract = async (req, res) => {
       res.status(500).send({ message: err });
       throw new Error("Abort");
     });
-
+  // Creating a new Abstract Record if it doesnt already exist
   if (abstract == null) {
     console.log("Creating a new Abstract Record.");
-    // ....add api call
+    // add api call
     abstract = new Abstract({
       title: req.body.title,
       url: req.body.url,
@@ -50,7 +71,7 @@ exports.requestAbstract = async (req, res) => {
     advancedTime: req.body.advancedTime,
     elementaryTime: req.body.elementaryTime,
   });
-
+  // interaction will be saved regardless of the existance of the abstract in databse
   await interaction
     .save()
     .then((interaction) => {
