4b202a1c998f38f1c84f738add7082b8b7ee3c97
nawzneen <earworm90@gmail.com>
2023-07-20 13:18:16 +0300

    model changed to gpt-4

--- Server-Side/controllers/abstract.controller.js
+++ Server-Side/controllers/abstract.controller.js
@@ -10,7 +10,7 @@ const feedback = require("../models/feedback");
 // fetch abstracts
 async function fetchResults(text, prompt) {
   const payload = {
-    model: "gpt-3.5-turbo",
+    model: "gpt-4",
     messages: [
       {
         role: "system",
@@ -44,18 +44,10 @@ async function fetchResults(text, prompt) {
     }
     const message = response.data.choices[0].message.content;
     let result = { message: message, status: "Ok" };
-
-    console.log("this is the result including message", result);
     return result;
-    // const data = response.data;
-    // console.log("this suppose to be the response data", data);
-
-    // res.status(200).send({ message: "Done" });
   } catch (error) {
     let result = { status: "Error" };
     return result;
-    // console.log(error);
-    // res.status(500).send({ error: "Something went wrong" });
   }
 }
 //
@@ -99,6 +91,9 @@ async function requestResults(req) {
 
   const elementaryPrompt =
     "Simplify the following abstract of a medical article while retaining the main idea. The target audience is individuals with an elementary school education degree. Use easy-to-understand language and avoid technical jargon and complex terms. You are allowed to summerize the text, but try not to summerize it too much. Ensure that the main idea of the original text is preserved without adding any additional information.  this is the abstract";
+
+  // const testPrompt =
+  //   "Simplify the following abstract of a medical article while retaining the main idea in two different versions for different target groups. use language that is understandble for the target audience.You should ensure that the main idea of the original text is preserved in the simplified versions.the first version should be for target audiance with elementary school degree. you are allowed to summerize the abstract while simplifying it, but try not to summerize it too much. you are allowed simplify the technical terms. the second version should be for target audiance with undergraduate university degree. Try not to summerize the text. you are allowed to keep some technical terms as long as they are not overly complicated. the output and the original abstract should be in one json file. first the first simplified version and then the second simplified version and the last should be the original abstract. this is the abstract:";
   // const elementaryPrompt =
   //   "Simplify the following text, using simplification level 2 out of 10. Simplification level 10 means no simplification, the output text should be exactly the same as the original text. Simplification level 1 means the output should be as simple as possible, written to an elementary school pupil. In other words, an elementary school pupil should be able to understand what the original text communicates. In any case, remember to retain key information in the text, but replace all jargon and complicated medical terminology into easier-to-read text. the vocabulary should be suitable for a kid in elementary level. for instance if the text mentions 'cardiovascular disease,' simplify it to 'heart disease':";
 
@@ -116,9 +111,9 @@ async function requestResults(req) {
     req.body.originalAbstract,
     elementaryPrompt
   );
+  const testResult = await fetchResults(req.body.originalAbstract, testPrompt);
   const titleResult = await fetchResults(req.body.originalTitle, titlePrompt);
 
-  console.log("result in test", advancedResult, elementaryResult, titleResult);
   // res.status(200).send({ message: "Done" });
   return {
     advancedAbstract: advancedResult.message,
