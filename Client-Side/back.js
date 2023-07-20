91167464fb06ca5b9b41585bfd7001463d5a9b68
nawzneen <earworm90@gmail.com>
2023-07-20 15:04:04 +0300

    set fetch from server

--- Client-Side/back.js
+++ Client-Side/back.js
@@ -1,3 +1,4 @@
+// import SERVER_URL from "config.js";
 chrome.runtime.onConnect.addListener(function (port) {
   if (port.name === "popupConnection") {
     port.onDisconnect.addListener(function () {
@@ -227,7 +228,10 @@ async function requestLogin(username) {
     body: JSON.stringify({ username }),
   };
   try {
-    var response = await fetch("http://localhost:8080/users/login", options);
+    var response = await fetch(
+      `http://86.50.229.149:8080/users/login`,
+      options
+    );
     response = await response.json();
     accessToken = response.accessToken;
   } catch (error) {
@@ -256,7 +260,7 @@ async function requestSummary(abstractInfromation) {
 
       try {
         const response = await fetch(
-          "http://localhost:8080/abstracts/abstract",
+          `http://86.50.229.149:8080/abstracts/abstract`,
           options
         );
         let responseData = await response.json();
@@ -295,7 +299,7 @@ async function requestStudyStatus() {
 
       try {
         const response = await fetch(
-          "http://localhost:8080/study/status",
+          `http://86.50.229.149:8080/study/status`,
           options
         );
         // console.log(response);
@@ -347,7 +351,7 @@ async function sendFeedback(feedback) {
       };
       try {
         const response = await fetch(
-          "http://localhost:8080/abstracts/submitFeedback",
+          `http://86.50.229.149:8080/abstracts/submitFeedback`,
           options
         );
         const responseData = await response.json();
